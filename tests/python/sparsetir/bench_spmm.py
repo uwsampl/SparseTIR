1a4beee8e3616043429fe6e35ccb91977c3ad64b
Zihao <expye@outlook.com>
2022-06-03 03:14:09 -0700

    remove old format

--- tests/python/sparsetir/bench_spmm.py
+++ tests/python/sparsetir/bench_spmm.py
@@ -1,5 +1,4 @@
 import dgl
-from sklearn.metrics import jaccard_score
 import tvm
 import tvm.testing
 import tvm.tir as tir
@@ -10,7 +9,7 @@ from tvm.script import tir as T
 from tvm.sparse import FormatRewriteRule, lower_sparse_buffer, lower_sparse_iter
 import tvm.sparse
 from ogb.nodeproppred import DglNodePropPredDataset
-from sparse_tir_format_rewrite_scripts import ell
+from sparse_tir_format_rewrite_scripts import ell, padding
 
 
 @T.prim_func
@@ -55,90 +54,6 @@ class TorchOpTimer(object):
         self.time = self.start_event.elapsed_time(self.end_event) / 1e3
 
 
-def pad_graph(g: dgl.DGLGraph, tile_size=32) -> dgl.DGLGraph:
-    u, v = g.edges()
-    rows = [u.flatten()]
-    cols = [v.flatten()]
-
-    for node_id, deg in enumerate(g.in_degrees().tolist()):
-        edges_to_add = ((deg + tile_size - 1) // tile_size) * tile_size - deg
-        rows.append(th.full((edges_to_add,), 0))
-        cols.append(th.full((edges_to_add,), node_id))
-
-    rows = th.cat(rows)
-    cols = th.cat(cols)
-
-    return dgl.graph((rows, cols), num_nodes=g.num_dst_nodes())
-
-
-@T.prim_func
-def csrmm_tir(
-    a: T.handle,
-    b: T.handle,
-    c: T.handle,
-    indptr: T.handle,
-    indices: T.handle,
-    M: T.int32,
-    N: T.int32,
-    K: T.int32,
-    NNZ: T.int32,
-) -> None:
-    T.func_attr({"global_symbol": "main", "tir.noalias": True})
-    A_data = T.match_buffer(a, (NNZ,), "float32")
-    B = T.match_buffer(b, (N * K,), "float32")
-    C = T.match_buffer(c, (M * K,), "float32")
-    A_indptr = T.match_buffer(indptr, (M + 1,), "int32")
-    A_indices = T.match_buffer(indices, (NNZ,), "int32")
-    for i, k in T.grid(M, K):
-        with T.block("spmm_outer"):
-            vi, vk = T.axis.remap("SS", [i, k])
-            with T.init():
-                C[vi * K + vk] = 0.0
-            for j in T.serial(0, A_indptr[vi + 1] - A_indptr[vi]):
-                with T.block("spmm_inner"):
-                    T.block_attr({"sparse": True})
-                    vj = T.axis.R(NNZ, j + A_indptr[vi])
-                    C[vi * K + vk] = C[vi * K + vk] + A_data[vj] * B[A_indices[vj] * K + vk]
-
-
-@T.prim_func
-def csrmm_padding_tir(
-    a: T.handle,
-    b: T.handle,
-    c: T.handle,
-    indptr: T.handle,
-    indices: T.handle,
-    M: T.int32,
-    N: T.int32,
-    K: T.int32,
-    NNZT: T.int32,
-    tile_size: T.int32,
-) -> None:
-    T.func_attr({"global_symbol": "main", "tir.noalias": True})
-    A_data = T.match_buffer(a, (NNZT * tile_size,), "float32")
-    B = T.match_buffer(b, (N * K,), "float32")
-    C = T.match_buffer(c, (M * K,), "float32")
-    A_indptr = T.match_buffer(indptr, (M + 1,), "int32")
-    A_indices = T.match_buffer(indices, (NNZT * tile_size,), "int32")
-    for i, k in T.grid(M, K):
-        with T.block("spmm_outer"):
-            vi, vk = T.axis.remap("SS", [i, k])
-            with T.init():
-                C[vi * K + vk] = 0.0
-            for j in T.grid(A_indptr[vi + 1] - A_indptr[vi]):
-                with T.block("spmm_inner"):
-                    T.block_attr({"sparse": True})
-                    vj = T.axis.remap("R", [j])
-                    for t in T.grid(tile_size):
-                        with T.block("spmm_inner_2"):
-                            vt = T.axis.remap("R", [t])
-                            C[vi * K + vk] = (
-                                C[vi * K + vk]
-                                + A_data[(vj + A_indptr[vi]) * tile_size + vt]
-                                * B[A_indices[(vj + A_indptr[vi]) * tile_size + vt] * K + vk]
-                            )
-
-
 def csr2ell_inv_index_map(o, i, j):
     return i, j
 
@@ -147,7 +62,7 @@ def csr2ell_index_map(i, j):
     return 0, i, j
 
 
-cached_formats = []
+cached_bucketing_format = []
 
 
 def bench_hyb(
@@ -159,7 +74,7 @@ def bench_hyb(
     cwm=2,
     column_part=1,
 ):
-    global cached_formats
+    global cached_bucketing_format
     mat = g.adj(transpose=True, scipy_fmt="csr")
     del g
     cwm = min(cwm, feat_size // 32)
@@ -244,22 +159,25 @@ def bench_hyb(
             sch.annotate(blk, "atomic", True)
             write_blk = sch.cache_write(blk, 0, "local")
             sch.reverse_compute_at(write_blk, fi, True)
-            ax = sch.get_loops(write_blk)[-1]
-            sch.unroll(ax)
-        sch.unroll(foi)
+            sch.unroll(sch.get_loops(write_blk)[-2])
         sch.bind(fi, "threadIdx.x")
         sch.bind(foo, "blockIdx.y")
+        sch.unroll(foi)
         sch.unroll(j)
         io, ii = sch.split(i, [None, max(1, bucket_sizes[-1] // bucket_size)])
         sch.bind(io, "blockIdx.x")
+        init_blk = sch.decompose_reduction(blk, fi)
+        ax0, ax1 = sch.get_loops(init_blk)[-2:]
+        sch.bind(ax0, "threadIdx.x")
+        sch.unroll(ax1)
 
     mod = tvm.sparse.lower_sparse_buffer(sch.mod)
     mod = tvm.tir.transform.RemoveUnusedArgs()(mod)
     f = tvm.build(mod, target="cuda")
 
     # prepare new formats
-    if len(cached_formats) > 0:
-        ell_indices, ell_a, ell_rows = cached_formats
+    if len(cached_bucketing_format) > 0:
+        ell_indices, ell_a, ell_rows = cached_bucketing_format
     else:
         ell_rows = []
         ell_indices = []
@@ -285,10 +203,10 @@ def bench_hyb(
                 a = np.zeros(
                     (ell_n[partition * len(bucket_sizes) + i], bucket_size), dtype=np.float32
                 )
-                for j, row in enumerate(ell_rows[partition * len(bucket_sizes) + i]):
-                    mat_row = sub_mat[row]
-                    indices[j, : mat_row.nnz] = mat_row.indices + partition * per_column_part_size
-                    a[j, : mat_row.nnz] = mat_row.data
+                for j, row_id in enumerate(ell_rows[partition * len(bucket_sizes) + i]):
+                    row = sub_mat[row_id]
+                    indices[j, : row.nnz] = row.indices + partition * per_column_part_size
+                    a[j, : row.nnz] = row.data
                 ell_indices.append(indices)
                 ell_a.append(a)
 
@@ -302,29 +220,29 @@ def bench_hyb(
             new_rows = np.zeros((ell_n[(partition + 1) * len(bucket_sizes) - 1],), dtype=np.int32)
             bucket_size = bucket_sizes[-1]
             i = 0
-            for row in ell_rows[-1]:
-                mat_row = sub_mat[row]
-                for start_offset in range(0, mat_row.nnz, bucket_size):
-                    if start_offset + bucket_size >= mat_row.nnz:
+            for row_id in ell_rows[-1]:
+                row = sub_mat[row_id]
+                for start_offset in range(0, row.nnz, bucket_size):
+                    if start_offset + bucket_size >= row.nnz:
                         # last bucket
-                        indices[i, : mat_row.nnz - start_offset] = (
-                            mat_row.indices[start_offset:] + partition * per_column_part_size
+                        indices[i, : row.nnz - start_offset] = (
+                            row.indices[start_offset:] + partition * per_column_part_size
                         )
-                        a[i, : mat_row.nnz - start_offset] = mat_row.data[start_offset:]
+                        a[i, : row.nnz - start_offset] = row.data[start_offset:]
                     else:
                         indices[i] = (
-                            mat_row.indices[start_offset : start_offset + bucket_size]
+                            row.indices[start_offset : start_offset + bucket_size]
                             + partition * per_column_part_size
                         )
                         a[i].fill(1)
-                    new_rows[i] = row
+                    new_rows[i] = row_id
                     i += 1
 
             ell_indices.append(indices)
             ell_a.append(a)
             ell_rows[-1] = new_rows
 
-        cached_formats = ell_indices, ell_a, ell_rows
+        cached_bucketing_format = ell_indices, ell_a, ell_rows
 
     # prepare nd array
     b_nd = tvm.nd.array(
@@ -349,135 +267,51 @@ def bench_hyb(
 
     # test accuracy
     f(*args)
-    row_idx = (np.abs(c_nd.numpy().reshape(-1, feat_size)[:, 1] - y_golden.numpy()[:, 1]) > 1e-3).nonzero()[0]
-    tvm.testing.assert_allclose(c_nd.numpy().reshape(-1, feat_size)[:, 1], y_golden.numpy()[:, 1], rtol=1e-4)
+    tvm.testing.assert_allclose(c_nd.numpy().reshape(-1, feat_size), y_golden.numpy(), rtol=1e-4)
 
     # evaluate time
     evaluator = f.time_evaluator(f.entry_name, tvm.cuda(0), number=10)
     print("tir hyb time: {:.3f}ms".format(evaluator(*args).mean * 1000))
 
 
-def bench_tir_csrmm(g, feat_size=128):
-    # generate random input
-    indptr, indices, _ = g.adj_sparse("csc")
+col_part_config = {"arxiv": 1, "proteins": 8, "pubmed": 1, "ppi": 8, "reddit": 8}
 
-    m = g.num_src_nodes()
-    n = g.num_dst_nodes()
-    k = feat_size
-    nnz = g.num_edges()
-    x = np.random.rand(n, k).astype("float32")
-    y = np.zeros((m * k,)).astype("float32")
+bucketing_config = {
+    "arxiv": [1, 2, 4, 8, 16, 32],
+    "proteins": [1, 2, 4, 8, 16, 32, 64, 128, 256],
+    "pubmed": [1, 2, 4, 8, 16, 32],
+    "ppi": [1, 2, 4, 8, 16, 32],
+    "reddit": [1, 2, 4, 8, 16, 32, 64, 128, 256, 512],
+}
 
-    # specialize function
-    _, _, _, _, _, M, N, K, NNZ = csrmm_tir.params
-    sch = tir.Schedule(csrmm_tir.specialize({M: m, N: n, K: k, NNZ: nnz}))
-    blk_outer = sch.get_block("spmm_outer")
-    i, k = sch.get_loops(blk_outer)
-    sch.bind(i, "blockIdx.x")
-    sch.bind(k, "threadIdx.x")
 
-    # convert numpy tensor to tvm ndarray
-    A_indptr = tvm.nd.array(indptr.numpy().astype("int32"), device=tvm.cuda(0))
-    A_indices = tvm.nd.array(indices.numpy().astype("int32"), device=tvm.cuda(0))
-    A_data = tvm.nd.array(np.ones((nnz,)).astype("float32"), device=tvm.cuda(0))
-    X_nd = tvm.nd.array(x.reshape(-1), device=tvm.cuda(0))
-    Y_nd = tvm.nd.array(y, device=tvm.cuda(0))
-
-    # build function
-    f = tvm.build(sch.mod, target="cuda")
-    accum_time = 0.0
-    runs = 0
-    cold_start_time = 3
-    for i in range(10):
-        with TorchOpTimer() as timer:
-            f(A_data, X_nd, Y_nd, A_indptr, A_indices)
-        if i >= cold_start_time:
-            accum_time += timer.time
-            runs += 1
-        Y_val = Y_nd.numpy()
-    print("tir naive time: {:.3f}ms".format(accum_time / runs * 1000))
-
-    g_gpu = g.to(0)
-    h = th.from_numpy(x).to(0)
-    weight = th.ones(nnz).to(0)
-    accum_time = 0.0
-    runs = 0
-    cold_start_time = 3
-    for i in range(10):
-        with TorchOpTimer() as timer:
-            out = dgl.ops.u_mul_e_sum(g_gpu, h, weight)
-        if i >= cold_start_time:
-            accum_time += timer.time
-            runs += 1
-    print("cusparse time: {:.3f}ms".format(accum_time / runs * 1000))
-
-    for tile_size in [4, 8, 16, 24, 32, 40, 48, 56, 64]:
-        g_pad = pad_graph(g, tile_size=tile_size)
-        m = g_pad.num_src_nodes()
-        n = g_pad.num_dst_nodes()
-        k = feat_size
-        x = np.random.rand(n, k).astype("float32")
-        y = np.zeros((m * k,)).astype("float32")
-        nnzt = g_pad.num_edges() // tile_size
-        indptr_, indices_, _ = g_pad.adj_sparse("csc")
-        _, _, _, _, _, M, N, K, NNZT, TILE_SIZE = csrmm_padding_tir.params
-        sch = tir.Schedule(
-            csrmm_padding_tir.specialize({M: m, N: n, K: k, NNZT: nnzt, TILE_SIZE: tile_size})
-        )
-        # print(sch.mod["main"].script())
-        blk_outer = sch.get_block("spmm_outer")
-        i, k = sch.get_loops(blk_outer)
-        koo, ko, ki = sch.split(k, [None, 2, 32])
-        blk_inner_2 = sch.get_block("spmm_inner_2")
-        (t,) = sch.get_loops(blk_inner_2)
-        sch.unroll(t)
-        sch.bind(ko, "vthread.x")
-        sch.bind(i, "blockIdx.x")
-        sch.bind(koo, "blockIdx.y")
-        sch.bind(ki, "threadIdx.x")
-
-        # convert numpy tensor to tvm ndarray
-        A_indptr = tvm.nd.array((indptr_.numpy() // tile_size).astype("int32"), device=tvm.cuda(0))
-        A_indices = tvm.nd.array(indices_.numpy().astype("int32"), device=tvm.cuda(0))
-        A_data = tvm.nd.array(np.ones((nnzt * tile_size,)).astype("float32"), device=tvm.cuda(0))
-        X_nd = tvm.nd.array(x.reshape(-1), device=tvm.cuda(0))
-        Y_nd = tvm.nd.array(y, device=tvm.cuda(0))
-
-        # build function
-        f = tvm.build(sch.mod, target="cuda")
-        # print(f.imported_modules[0].get_source())
-        accum_time = 0.0
-        runs = 0
-        cold_start_time = 3
-        for i in range(10):
-            with TorchOpTimer() as timer:
-                f(A_data, X_nd, Y_nd, A_indptr, A_indices)
-            if i >= cold_start_time:
-                accum_time += timer.time
-                runs += 1
-            Y_val = Y_nd.numpy()
-        print(
-            "tir w/ padding (tile_size={}) time: {:.3f}ms".format(
-                tile_size, accum_time / runs * 1000
-            )
-        )
+def get_dataset(name: str):
+    if name == "arxiv":
+        arxiv = DglNodePropPredDataset(name="ogbn-arxiv")
+        g = arxiv[0][0]
+    elif name == "proteins":
+        proteins = DglNodePropPredDataset(name="ogbn-proteins")
+        g = proteins[0][0]
+    elif name == "pubmed":
+        pubmed = dgl.data.PubmedGraphDataset()
+        g = pubmed[0]
+    elif name == "ppi":
+        ppi = dgl.data.PPIDataset()
+        g = dgl.batch(ppi)
+    elif name == "reddit":
+        reddit = dgl.data.RedditDataset()
+        g = reddit[0]
+    else:
+        raise KeyError("Unknown dataset {}.".format(name))
+    return g.int()
 
 
 if __name__ == "__main__":
-    # arxiv = DglNodePropPredDataset(name="ogbn-arxiv")
-    # g = arxiv[0][0] # [1, 2, 4, 8, 16, 32]
-    # proteins = DglNodePropPredDataset(name="ogbn-proteins")
-    # g = proteins[0][0]
-    # pubmed = dgl.data.PubmedGraphDataset()
-    # g = pubmed[0] # [1, 2, 4, 8, 16, 32]
-    ppi = dgl.data.PPIDataset()
-    g = dgl.batch(ppi)  # [1, 2, 4, 8, 16, 32]
-    # reddit = dgl.data.RedditDataset()
-    # g = reddit[0] # [64, 128, 256, 512]
+    name = "arxiv"
+    g = get_dataset(name)
 
-    g = g.int()
     for feat_size in [32, 64, 128, 256, 512]:
-        print("feat_size=", feat_size)
+        print("feat_size =", feat_size)
         x = th.rand((g.num_src_nodes(), feat_size))
         y_golden = dgl.ops.copy_u_sum(g, x)
         bench_hyb(
@@ -485,10 +319,7 @@ if __name__ == "__main__":
             x,
             y_golden,
             feat_size=feat_size,
-            bucket_sizes=[1, 2, 4, 8, 16, 32, 64],
+            bucket_sizes=bucketing_config[name],
             cwm=2,
-            column_part=8,
+            column_part=col_part_config[name],
         )
-    for feat_size in [32, 64, 128, 256, 512]:
-        print("feat_size=", feat_size)
-        bench_tir_csrmm(g, feat_size=feat_size)
