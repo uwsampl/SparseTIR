a1913f3aceabd87669eee04917f2de21a9aa0506
Zihao <expye@outlook.com>
2022-11-18 18:03:33 -0800

    add sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,10 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.mathjax',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
