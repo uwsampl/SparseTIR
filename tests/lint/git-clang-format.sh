4bb05714e462ec6d97eea8b985b2b55bf00f2d7f
Zihao Ye <expye@outlook.com>
2022-12-14 16:12:55 +0800

    [Hotfix] Fix the clang-format installation in Dockerfile (#81)
    * hotfix
    
    * fix
    
    * fix
    
    * clang-format-14
    
    * upd

--- tests/lint/git-clang-format.sh
+++ tests/lint/git-clang-format.sh
@@ -55,15 +55,10 @@ cleanup()
 }
 trap cleanup 0
 
-CLANG_FORMAT=clang-format-10
-
-if [ -x "$(command -v clang-format-10)" ]; then
-    CLANG_FORMAT=clang-format-10
-elif [ -x "$(command -v clang-format)" ]; then
-    echo "clang-format might be different from clang-format-10, expect potential difference."
-    CLANG_FORMAT=clang-format
+if [ -x "$(command -v clang-format-14)" ]; then
+    CLANG_FORMAT=clang-format-14
 else
-    echo "Cannot find clang-format-10"
+    echo "Cannot find clang-format-14"
     exit 1
 fi
 
