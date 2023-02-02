1ed1299ab0374f2acc0898190b5b42a6aab3f45d
Zihao <expye@outlook.com>
2023-02-01 19:11:00 -0800

    upd

--- docker/install/ubuntu2004_install_llvm.sh
+++ docker/install/ubuntu2004_install_llvm.sh
@@ -31,4 +31,4 @@ wget -q -O - http://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 
 apt-get update && apt-get install -y \
      llvm-14 clang-format-14 \
-     clang-14 libclang-14-dev libpolly-14-dev libclang-common-14-dev
+     clang-14 libclang-14-dev libclang-common-14-dev
